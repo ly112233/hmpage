b58b4f1940f6294e5c69f98472c422cc1c0c4219
l443124398 <443124398@qq.com>
2020-07-10 10:02:16 +0800

    加入相应目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
 <div>
-123
-
+    <router-view></router-view>
 </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// import {router} from '@/router/index.js'
+import router from '@/router/index.js'
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
